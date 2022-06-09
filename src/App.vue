439dd637117fede6df75aa4621a98a7ba54349c6
cramzhou <zhoumingye@kedacom.com>
2022-06-09 14:38:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><el-button type="primary">Default</el-button></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const aaa = ref(1);
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
